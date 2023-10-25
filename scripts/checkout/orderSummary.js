ec4635c1f4eb4800723f97fd20d2506fadc33637
Rashed112 <rashedzaman319@gmail.com>
2023-10-25 16:05:19 +0600

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,10 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
 
@@ -111,6 +108,8 @@ export function renderOrderSummary(){
 
                 const container = document.querySelector(`.js-cart-item-container-${productId}`);
                 container.remove();
+
+                renderPaymentSummary();
             });
         });
 
@@ -119,6 +118,7 @@ export function renderOrderSummary(){
                 const {productId, deliveryOptionId} = element.dataset;
                 updateDeliveryOption(productId, deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             });
         });
     }
